1736cc1e1c5efab048e040c5d371b8ac1764e969
ccckmit <ccckmit@gmail.com>
2018-04-01 09:00:26 +0800

    add nn6.Graph class

--- test/optimizeTest.js
+++ test/optimizeTest.js
@@ -2,26 +2,26 @@ const nn6 = require('../lib/nn6')
 
 // f(x,y) = x^2 + 2xy + y^2 + 2
 // 在 x=0 y=0 時，有最小值 2
-class Net1 extends nn6.Net {
+class Graph1 extends nn6.Graph {
   constructor () {
     super()
-    let v = this.vars = nn6.Graph.newVariables(['x', 'y', 'xx', '_2xy', 'yy', 'f'])
+    let v = this.addVariables(['x', 'y', 'xx', '_2xy', 'yy', 'f'])
     let [c2] = nn6.Graph.newConstants([2])
-    this.inputs = [v.x, v.y]
+    this.addInputs([v.x, v.y])
     v.x.value = 1
     v.y.value = 2
-    this.out = v.f
-    this.gates = [
+    this.setOutput(v.f)
+    this.addGates([
       new nn6.Times([v.x, v.x], v.xx),
       new nn6.Times([c2, v.x, v.y], v._2xy),
       new nn6.Times([v.y, v.y], v.yy),
       new nn6.Sum([v.xx, v._2xy, v.yy, c2], v.f)
-    ]
+    ])
     console.log('vars=%j', this.vars)
   }
 }
 
-let net = new Net1()
+let net = new nn6.Net(new Graph1())
 net.run(300)
 
   /*
